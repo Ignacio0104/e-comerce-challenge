67ea477623bb7c2c84f975bc0678f497448b27fb
Ignacio Smirlian <ignacio.smirlian@globant.com>
2023-04-27 17:02:49 -0300

    info container responsive

--- src/components/pure/ImageCards.tsx
+++ src/components/pure/ImageCards.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./ImageCards.css";
+import PrevButton from "../../assets/icon-previous.svg";
+import NextButton from "../../assets/icon-next.svg";
 
 interface IImageProps {
   images: string[];
@@ -9,13 +11,27 @@ const ImageCards = ({ images }: IImageProps) => {
   const [mainImage, setMainImage] = useState(images[0]);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const changePicture = (index: number) => {
-    setMainImage(images[index]);
-    setSelectedIndex(index);
+    if (index >= 0 && index < images.length) {
+      setMainImage(images[index]);
+      setSelectedIndex(index);
+    }
   };
   return (
     <div className="main-container">
       <img className="main-pic" src={mainImage} alt="main" />
       <div className="image-selector">
+        <button
+          className="prev-btn"
+          onClick={() => changePicture(selectedIndex - 1)}
+        >
+          <img src={PrevButton} alt="previous" />
+        </button>
+        <button
+          className="next-btn"
+          onClick={() => changePicture(selectedIndex + 1)}
+        >
+          <img src={NextButton} alt="next" />
+        </button>
         {images.map((image, index) => (
           <img
             className={`thumbnail ${selectedIndex === index ? "active " : ""}`}
